45f5b0fc804e81ec00d522816a90701e486d1151
SahilS-F60 <sahil@pentagoat.com>
2024-12-13 12:40:16 +0530

    Part 8: Manipulating Response Data

--- src/features/users/UserPage.tsx
+++ src/features/users/UserPage.tsx
@@ -1,15 +1,38 @@
 import { Link, useParams } from 'react-router-dom';
+import { createSelector } from '@reduxjs/toolkit';
+import type { TypedUseQueryStateResult } from '@reduxjs/toolkit/query/react';
 
 import { useAppSelector } from '@/app/hooks';
 import { selectPostsByUser } from '../posts/postsSlice';
+
+import { useGetPostsQuery, Post } from '../api/apiSlice';
+
 import { selectUserById } from './usersSlice';
+
+// Create a TS type that represents "the result value passed
+// into the `selectFromResult` function for this hook"
+type GetPostSelectFromResultArg = TypedUseQueryStateResult<Post[], any, any>;
+
+const selectPostsForUser = createSelector(
+  (res: GetPostSelectFromResultArg) => res.data,
+  (res: GetPostSelectFromResultArg, userId: string) => userId,
+  (data, userId) => data?.filter((post) => post.user === userId),
+);
 
 export default function UserPage() {
   const { userId } = useParams();
 
+  // - RTK
   const user = useAppSelector((state) => selectUserById(state, userId));
+  // const postsForUser = useAppSelector((state) => selectPostsByUser(state, userId));
 
-  const postsForUser = useAppSelector((state) => selectPostsByUser(state, userId));
+  // - RTKQ
+  const { postsForUser } = useGetPostsQuery(undefined, {
+    selectFromResult: (result) => ({
+      ...result,
+      postsForUser: selectPostsForUser(result, userId!),
+    }),
+  });
 
   if (!user) {
     return (
@@ -19,7 +42,7 @@ export default function UserPage() {
     );
   }
 
-  const postTitles = postsForUser.map((post) => (
+  const postTitles = postsForUser?.map((post) => (
     <li key={post.id}>
       <Link to={`/posts/${post.id}`}>{post.title}</Link>
     </li>
--- src/features/users/usersSlice.ts
+++ src/features/users/usersSlice.ts
@@ -1,4 +1,4 @@
-import { createSlice, createEntityAdapter, createSelector } from '@reduxjs/toolkit';
+import { createSlice, createEntityAdapter, EntityState, createSelector } from '@reduxjs/toolkit';
 
 import { client } from '@/api/client';
 import { apiSlice } from '../api/apiSlice';
@@ -50,25 +50,32 @@ export const selectCurrentUser = (state: RootState) => {
 // -- RTKQ query
 export const apiSliceWithUsers = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
-    getUsers: builder.query<User[], void>({
+    getUsers: builder.query<EntityState<User, string>, void>({
       query: () => '/users',
+      transformResponse: (res: User[]) => {
+        return usersAdapter.setAll(initialState, res);
+      },
     }),
   }),
 });
 
 export const { useGetUsersQuery } = apiSliceWithUsers;
 
+// Calling `someEndpoint.select(someArg)` generates a new selector that will return
+// the query result object for a query with those parameters.
+// To generate a selector for a specific query argument, call `select(theQueryArg)`.
+// In this case, the users query has no params, so we don't pass anything to select()
 const emptyUsers: User[] = [];
 
 export const selectUsersResult = apiSliceWithUsers.endpoints.getUsers.select();
 
-export const selectAllUsers = createSelector(selectUsersResult, (usersResult) => usersResult?.data ?? emptyUsers);
+export const selectUsersData = createSelector(selectUsersResult, (result) => result?.data ?? initialState);
 
-export const selectUserById = createSelector(
-  selectAllUsers,
-  (state: RootState, userId: string) => userId,
-  (user, userId) => user.find((user) => user.id === userId),
-);
+// export const selectUserById = createSelector(
+//   selectAllUsers,
+//   (state: RootState, userId: string) => userId,
+//   (user, userId) => user.find((user) => user.id === userId),
+// );
 
 export const selectCurrentUser = (state: RootState) => {
   const currentUsername = selectCurrentUsername(state);
@@ -76,3 +83,5 @@ export const selectCurrentUser = (state: RootState) => {
     return selectUserById(state, currentUsername);
   }
 };
+
+export const { selectAll: selectAllUsers, selectById: selectUserById } = usersAdapter.getSelectors(selectUsersData);
